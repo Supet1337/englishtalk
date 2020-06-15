7e48282158a0fe4a0d9628bc96ebb08360b3fc1a
Рубинов Максим <maksrubinov@mail.ru>
2020-06-15 22:42:18 +0300

    Templates

--- englishtalk/settings.py
+++ englishtalk/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rv(-m=hx@341@qi-$kqodugk+^#rrbiz!wq4ynxjsifjq^!3xr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'englishtalk.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['talkapp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+
+STATIC_ROOT = "./collectedstatic"
+
+SECURE_SSL_REDIRECT = False
