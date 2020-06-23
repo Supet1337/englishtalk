ddc270fcbd520831339879a464f7dfce98030f88
Maksim Rubinov <maksrubinov@mail.ru>
2020-06-23 17:46:59 +0300

    Курсы

--- talkapp/urls.py
+++ talkapp/urls.py
@@ -3,7 +3,21 @@ from .views import *
 
 urlpatterns = [
     path('', index),
-    path('profile', profile),
+    path('dashboard', dashboard),
     path('courses', courses),
-    path('about', about),
+    path('course/<ielts', ielts),
+    path('course/interview', interview),
+    path('course/freespeaking', freespeaking),
+    path('course/it', it),
+    path('course/travel', travel),
+    path('course/business', business),
+    path('course/exam', exam),
+    path('course/engineer', engineer),
+    path('course/teen', teen),
+    path('course/child', child),
+    path('course/personally', personally),
+    path('course/anylevel', anylevel),
+    path('course/grammar', grammar),
+
+
 ]
--- talkapp/views.py
+++ talkapp/views.py
@@ -3,11 +3,49 @@ from django.shortcuts import render, redirect
 def index(request):
     return render(request,'index.html')
 
-def profile(request):
-    return render(request,'profile.html')
+def dashboard(request):
+    return render(request,'dashboard.html')
 
 def courses(request):
     return render(request,'courses.html')
 
-def about(request):
-    return render(request,'about.html')
+def ielts(request):
+    return render(request,'courses/ielts.html')
+
+def interview(request):
+    return render(request,'courses/interview.html')
+
+def freespeaking(request):
+    return render(request,'courses/freespeaking.html')
+
+def it(request):
+    return render(request,'courses/it.html')
+
+def travel(request):
+    return render(request,'courses/travel.html')
+
+def business(request):
+    return render(request,'courses/business.html')
+
+def exam(request):
+    return render(request,'courses/exam.html')
+
+def engineer(request):
+    return render(request,'courses/engineer.html')
+
+def teen(request):
+    return render(request,'courses/teen.html')
+
+def child(request):
+    return render(request,'courses/child.html')
+
+def personally(request):
+    return render(request,'courses/personally.html')
+
+def anylevel(request):
+    return render(request,'courses/anylevel.html')
+
+def grammar(request):
+    return render(request,'courses/grammar.html')
+
+
