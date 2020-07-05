810691158e11e8cc11a006d69a5ae37dfbf630b5
Maksim Rubinov <maksrubinov@mail.ru>
2020-07-05 15:55:54 +0300

    upd

--- englishtalk/settings.py
+++ englishtalk/settings.py
@@ -152,7 +152,6 @@ if DEBUG:
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 ACCOUNT_EMAIL_VERIFICATION = "none"
 EMAIL_HOST_USER = 'noreply.englishtalk@gmail.com'
