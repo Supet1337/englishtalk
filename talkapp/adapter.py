eb75979c6130d17d784a5e6a5b67a122de2e9552
Maksim Rubinov <maksrubinov@mail.ru>
2021-07-05 17:09:27 +0300

    reg

--- talkapp/adapter.py
+++ talkapp/adapter.py
@@ -30,15 +30,15 @@ class SocialAccountAdapter(DefaultSocialAccountAdapter):
         roomName = get_random_string(length=32)
         user_add.user = user
         user_add.video_chat = roomName
-        s = str(user.socialaccount_set.filter(provider='vk')[0].extra_data['bdate'])
-        if s:
+        try:
+            s = str(user.socialaccount_set.filter(provider='vk')[0].extra_data['bdate'])
             s = s.split('.')
             s1 = f"{s[2]}-{s[1]}-{s[0]}"
             user_add.birthday = s1
-
+        except:
+            pass
         room.name = roomName
         room.student = user
-
         # user_add.image = user.socialaccount_set.filter(provider='vk')[0].extra_data['photo']
         user_add.save()
         room.save()
