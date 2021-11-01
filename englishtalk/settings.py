27f2092ef5a700b5a0ec658d9e690f104f11f4b0
Petr Vorontsov <Petr.Vorontsov2003@gmail.com>
2021-11-01 17:19:12 +0300

    Корректировки 28/35

--- englishtalk/settings.py
+++ englishtalk/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'rv(-m=hx@341@qi-$kqodugk+^#rrbiz!wq4ynxjsifjq^!3xr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
